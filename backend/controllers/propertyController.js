8b22c1b943564911355b7b4bab6ae3c3468ed841
Elizebath <elizebaththomasv@gmail.com>
2025-02-21 17:05:03 +0530

    Added Complaint Module

--- backend/controllers/propertyController.js
+++ backend/controllers/propertyController.js
@@ -4,6 +4,10 @@ const asyncHandler = require("express-async-handler");
 const propertyController = {
     createProperty: asyncHandler(async (req, res) => {
         const newProperty = new Property(req.body);
+        const encodedAddress = encodeURIComponent(address);
+        
+        const googleMapsUrl = `https://www.google.com/maps/dir/?api=1&destination=${encodedAddress}&travelmode=driving`;
+        
         const savedProperty = await newProperty.save();
         res.status(201).send(savedProperty);
     }),
